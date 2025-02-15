9a6a432ab4fc8691ceb41b4cf5d8c442b64753d9
Graziela <grazielabitencourt.aluno@unipampa.edu.br>
2025-02-15 01:08:15 -0300

    Pagina de login feita

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// Adiciona um evento de escuta ao objeto "window" para detectar a rolagem da página até o tamanho 400
 window.addEventListener("scroll", function(){
     let header = this.document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 400)
